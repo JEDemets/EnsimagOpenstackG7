a9d37adce299008200d6f8a06d074f5bca24f93d
Jules-Eugène Demets <jules-eugene.dmets@ensimag.grenoble-inp.fr>
2017-01-09 15:55:29 +0100

    mains.sh install apache2 & php5 : working

--- main.sh
+++ main.sh
@@ -1,2 +1,6 @@
  #!/bin/bash
-ap-get install apache2
+apt-get install -y apache2
+apt-get install -y libapache2-mod-php5
+apt-get update --fix-missing
+apt-get install -y php5
+#missing : copu to /var/www/html
