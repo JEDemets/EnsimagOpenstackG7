0c1a59e820c14eb37c341f5d99bd01206bf5f276
Jules-Eugène Demets <jules-eugene.dmets@ensimag.grenoble-inp.fr>
2017-01-18 11:53:21 +0100

    correct server address

--- host.sh
+++ host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $1 "	server_id"  >> /etc/hosts
-echo $2 "	serveur_worker"  >> /etc/hosts
+echo $2 "	serveur_button"  >> /etc/hosts
 echo $3 "	serveur_status" >> /etc/hosts
 echo $4 "	serveur_picture">> /etc/hosts
 
