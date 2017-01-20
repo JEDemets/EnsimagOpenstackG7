3dba12ee2b2fc5670e9bbadc3a277b3025d26d15
MorenoLaQuatra <moreno.laquatra@gmail.com>
2017-01-20 19:19:34 +0100

    added library and curl

--- picture.sh
+++ picture.sh
@@ -4,8 +4,15 @@ apt-get install -y libapache2-mod-php5
 apt-get update --fix-missing
 apt-get install -y php5
 apt-get install -y php5-curl
+apt-get install -y curl
 service apache2 restart
 rm /var/www/html/index.html
 cd /tmp/app/EnsimagOpenstackG7-application_structure/
 echo "$1" > ./server_picture/address.swift
 cp ./server_picture/* /var/www/html/
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+composer --version
+composer require rackspace/php-opencloud
+mkdir /var/www/html/vendor/
+cp -r ./vendor/* /var/www/html/vendor/
