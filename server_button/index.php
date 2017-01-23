36a6987665f99fc83a469486c9966ac6e6df09e7
Moreno La Quatra <moreno.laquatra@gmail.com>
2017-01-23 11:34:43 +0100

    update for url/psw/containername....

--- server_button/index.php
+++ server_button/index.php
@@ -44,11 +44,36 @@
 
     $address_swift = file_get_contents("address.swift");
     $address_swift = trim(preg_replace('/\s\s+/', ' ', $address_swift));
+<<<<<<< Updated upstream
     $authUrl = 'http://10.11.50.26:5000/v2.0';
   	$username = 'groupe7';
   	$password = 'dzCr8tliSyo='; // mot de passe Horizon
   	$tenant = 'project7';
     $swiftUrl = file_get_contents("address.swift");
+=======
+
+    $authUrl = file_get_contents("auth.url");
+    $authUrl = trim(preg_replace('/\s\s+/', ' ', $authUrl));
+    //$authUrl = 'http://10.11.50.26:5000/v2.0';
+
+    $username = file_get_contents("user.name");
+    $username = trim(preg_replace('/\s\s+/', ' ', $username));
+    //$username = 'groupe7';
+
+    $password = file_get_contents("pwd");
+    $password = trim(preg_replace('/\s\s+/', ' ', $password));
+    //$password = 'dzCr8tliSyo=';
+
+    $tenant = file_get_contents("tenant");
+    $tenant = trim(preg_replace('/\s\s+/', ' ', $tenant));
+    //$tenant = 'project7';
+
+    $name_container = file_get_contents("container.name");
+    $name_container = trim(preg_replace('/\s\s+/', ' ', $name_container));
+    //$name_container = "test";
+
+    //$swiftUrl = 'http://10.11.50.26:8080/v1/AUTH_2db62f6fa1664823bddbf1e03d35f0b4';
+>>>>>>> Stashed changes
   	$serviceName = 'swift';
   	$region = 'region1';
 
@@ -61,7 +86,7 @@
 
     $client->authenticate();
     $service = $client->objectStoreService($serviceName, $region);
-  	$container = $service->getContainer('test');
+  	$container = $service->getContainer($name_container);
 
   	$container->uploadObject($user_id, $output);
 
