9c2e464e9c7744a8f5ab7a4fef6d173ed9101095
Jules-Eugène Demets <jules-eugene.dmets@ensimag.grenoble-inp.fr>
2017-01-24 16:55:46 +0100

    fixing imagemagick install

--- worker.sh
+++ worker.sh
@@ -2,5 +2,7 @@
 
 apt-get install -y python3 python3-flask
 apt-get install -y Imagemagick
+apt-get update
+apt-get install -y imagemagick
 cd /tmp/app/EnsimagOpenstackG7-application_structure/w
 ./w.py &
